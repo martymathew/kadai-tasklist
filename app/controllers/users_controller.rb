36fcb9694562dc89471edf349ff789610e4f522b
marty <icespray009@gmail.com>
2018-09-09 08:42:59 +0000

    revise user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
-  before_action :current_user, 
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     @users = User.all.page(params[:page])
@@ -33,4 +33,13 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end 
 end
+
